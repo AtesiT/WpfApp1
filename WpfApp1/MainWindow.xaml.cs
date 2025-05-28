a49077e9f929c5ce0de4b8156a6e4173dc2fe846
AtesiT <atesit@mail.ru>
2025-05-28 17:20:14 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySqlConnector;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MySqlConnection _connection;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _connection = new MySqlConnection("Server=localhost;User ID=root;Password=student;Database=demoapp");
+            _connection.Open();
+
+        }
+
+        public void AuthButton_Click(Object sender, RoutedEventArgs e)
+        {
+            string Login = loginTextBox.Text.Trim();
+            string Password = passwordTextBox.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(Login)
+                || string.IsNullOrWhiteSpace(Password))
+            {
+                MessageBox.Show("Введите логин и пароль!", "Ошибка");
+                return;
+            }
+
+            try
+            {
+                var command = _connection.CreateCommand();
+                command.CommandText = "SELECT COUNT(*) FROM users WHERE login = @login AND password = @password";
+                command.Parameters.AddWithValue("login", Login);
+                command.Parameters.AddWithValue("password", Password);
+
+                var result = (long)command.ExecuteScalar();
+
+                if (result > 0)
+
+                    MessageBox.Show("Успешная авторизация!");
+                else
+                    MessageBox.Show("Неверный логин или пароль");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Не удалось подключиться к базе данных!" + ex.Message, "База данных");
+            }
+        }
+
+        public void RefillButton_Click(Object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //var deleteCommand = _connection.CreateCommand();
+                //deleteCommand.CommandText = "DELETE from users";
+                //deleteCommand.ExecuteNonQuery();
+
+                var command = _connection.CreateCommand();
+                command.CommandText = @"INSERT INTO users (login, password) VALUES
+                    ('admin','super'),
+                    ('manager','owner');
+                ";
+                int rows = command.ExecuteNonQuery();
+
+                MessageBox.Show($"База перезаполнена. Добавлено пользователей: {rows}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при заполнении базы: {ex.Message}");
+            }
+        }
+    }
+}
